aa4574c05eb0635998e0a81c0185e61664d9aa46
okulovsky <yuri.okulovsky@gmail.com>
2014-06-27 11:42:35 +0600

    fixes in Lecture01

--- CS/Модуль 01 - Введение в C Sharp/Лекция 01 - Hello, world!/S04 - Строки.cs
+++ CS/Модуль 01 - Введение в C Sharp/Лекция 01 - Hello, world!/S04 - Строки.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Slide05
+namespace Slide04
 {
     class Program
     {
@@ -13,7 +13,8 @@ namespace Slide05
             //Строки - это последовательности символов
             string myString = "Hello, world!";
         
-            //У строк есть свои собственные методы и "переменные" (правильно называть это свойствами), которые позволяют узнать информацию. 
+            //У строк есть свои собственные методы и "переменные" (правильно называть это свойствами), 
+            //которые позволяют узнать информацию о строке 
             Console.WriteLine(myString.Length);
 
             myString = myString.Substring(0, 5);
---
+++ CS/Модуль 01 - Введение в C Sharp/Лекция 01 - Hello, world!/S09 - Значения переменных по умолчанию.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slide09
+{
+    class Program
+    {
+        static string unassignedStringVariable;
+        static int unassignedIntVariable;
+
+        static void Main()
+        {
+            int localVariable;
+            //Console.WriteLine(localVariable.ToString());
+            // так нельзя! Значение переменной неопределено.
+
+            Console.WriteLine(unassignedIntVariable.ToString());
+            //так можно. Глобальные переменные инициализированы значением по умолчанию. Для int это 0.
+
+            Console.WriteLine(unassignedStringVariable.ToString());
+            //это вызовет ошибку. Значение по умолчанию для string - это null, а не пустая строка.
+            //Переменная не инициализирована, и нельзя вызывать ее методы
+
+            Console.WriteLine(unassignedStringVariable);
+            //Интересно, что так можно, потому что метод WriteLine проверяет, не является ли 
+            //unassignedStringVariable null, и печатает пустую строку, если является.
+
+        }
+    }
+}
