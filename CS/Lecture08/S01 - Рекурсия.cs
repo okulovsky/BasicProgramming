6bb87fe1c7feb0efb2445d52f67439452510f901
okulovsky <yuri.okulovsky@gmail.com>
2014-08-19 15:19:07 +0600

    Лекции 8-9

---
+++ CS/Lecture08/S01 - Рекурсия.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Slide02
+{
+    public class Program
+    {
+        static void Make(int n)
+        {
+            for (int i = n-1; i >=0; i--)
+            {
+                Console.Write(i.ToString() + " ");
+                Make(i);
+            }
+        }
+
+        public static void Main()
+        {
+            Console.Write("Make(1): ");
+            Make(1);
+            Console.WriteLine();
+
+            Console.Write("Make(2): ");
+            Make(2);
+            Console.WriteLine();
+
+            Console.Write("Make(3): ");
+            Make(6);
+            Console.WriteLine();
+        }
+    }
+}
