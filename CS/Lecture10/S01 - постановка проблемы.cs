d9a2f3df0ea510364cc13ac92c72261e763eeb58
okulovsky <yuri.okulovsky@gmail.com>
2014-09-08 14:19:58 +0600

    + lecture 10

---
+++ CS/Lecture10/S01 - постановка проблемы.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Slide01
+{
+    //Допустим, мы хотим хранить список студентов и печатать его элементы
+    //Пока эту проблему можно решить так:
+    public class Program
+    {
+        static void PrintStudent(string firstName, string lastName)
+        {
+            Console.WriteLine("{0,-15}, {1}", firstName, lastName);
+        }
+
+        static string[] firstNames;
+        static string[] lastNames;
+
+        static void Main()
+        {
+            firstNames = new string[2];
+            lastNames = new string[2];
+            firstNames[0] = "John";
+            firstNames[1] = "William";
+            lastNames[0] = "Jones";
+            lastNames[1] = "Williams";
+            PrintStudent(firstNames[0], lastNames[0]);
+            PrintStudent(firstNames[1], lastNames[1]);
+        }
+        /* Это плохо:
+         * - нет никакой синтаксической гарантии, что firstNames и lastNames одной длины,
+         *   что по одному индексу хранятся данные об одном человеке
+         * - при добавлении новой информации о студенте (например, bool dormitoryRequired)
+         *   придется переписывать всю программу
+         * - при большом количестве новой информации, количество аргументов PrintStudent
+         *   будет большим, а вызов этого метода станет неудобным.
+         */
+
+    }
+}
