30c99da57990e42e9601b2228f27d7be8bccf72f
okulovsky <yuri.okulovsky@gmail.com>
2014-09-28 13:59:40 +0600

    + lecture 12

---
+++ CS/Lecture12a/S03 - отложенные ошибки.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Slide03
+{
+    public class Transaction
+    {
+        public int DepartmentId;
+        public double Profit;
+    }
+
+
+    public class ReportData
+    {
+        public int DepartmentsCount;
+        public List<Transaction> Transactions = new List<Transaction>();
+        public void PrintProfits()
+        {
+            var profits = new double[DepartmentsCount];
+            foreach (var e in Transactions)
+                profits[e.DepartmentId] += e.Profit;
+            for (int i = 0; i < DepartmentsCount; i++)
+                Console.WriteLine("{0,-10}{1}", i, profits[i]);
+        }
+    }
+
+    public class Program
+    {
+        public static void MainX()
+        {
+            var report = new ReportData
+            {
+                DepartmentsCount = 2,
+                Transactions = {
+                    new Transaction { DepartmentId=0, Profit=10000 },
+                    new Transaction { DepartmentId=1, Profit=20000 },
+                    new Transaction { DepartmentId=1, Profit=10000 }
+                }
+            };
+            report.PrintProfits();
+
+            //но что, если кто-то напишет так?
+            report.DepartmentsCount = -1;
+            report.PrintProfits();
+
+        }
+
+    }
+}
---
+++ CS/Lecture12a/S04 - защита методами.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Slide04
+{
+    public class Transaction
+    {
+        public int DepartmentId;
+        public double Profit;
+    }
+
+
+    public class ReportData
+    {
+        private int departmentsCount;
+
+        public int GetDepartmentsCount() { return departmentsCount; }
+        public void SetDepartmentsCount(int value)
+        {
+            if (value < 0) throw new ArgumentException();
+            departmentsCount = value;
+        }
+
+        public List<Transaction> Transactions = new List<Transaction>();
+        public void PrintProfits()
+        {
+            var profits = new double[departmentsCount];
+            foreach (var e in Transactions)
+                profits[e.DepartmentId] += e.Profit;
+            for (int i = 0; i < departmentsCount; i++)
+                Console.WriteLine("{0,-10}{1}", i, profits[i]);
+        }
+    }
+
+    public class Program
+    {
+        public static void MainX()
+        {
+            var report = new ReportData
+            {
+                // departmentCount=2, -- так писать нельзя, ведь поле приватное
+                Transactions = {
+                    new Transaction { DepartmentId=0, Profit=10000 },
+                    new Transaction { DepartmentId=1, Profit=20000 },
+                    new Transaction { DepartmentId=1, Profit=10000 }
+                }
+            };
+            report.SetDepartmentsCount(2); //поэтому придется так.
+            report.PrintProfits();
+
+            //но что, если кто-то напишет так?
+            //report.departmentsCount = -1; - так теперь написать нельзя
+            report.SetDepartmentsCount(-1); //так написать можно, но ошибка возникнет сразу же.
+            
+            // [....]
+            // Учтите, что в реальной разработке в этом месте может пройти несколько часов действий пользователя
+            report.PrintProfits(); // и ошибка произойдет теперь не в самом конце, а в момент ее совершения.
+
+            // Все прекрасно, но как написать report.departmentsCount++? Вот так:
+            report.SetDepartmentsCount(report.GetDepartmentsCount() + 1);
+            // Слегка неизящно.
+
+            // То, что защита целостности выглядит извне сложно и неизящно 
+            // мешает программисту ее использовать, и программы получаются плохими.
+        }
+
+    }
+}
