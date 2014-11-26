a01c68134c13d2aca098e3b4a0c2938ee8ab7dd4
okulovsky <yuri.okulovsky@gmail.com>
2014-11-26 11:28:21 +0500

    Проспект всех лекций 2 семестра

---
+++ CS/Lecture18/S06 - LINQ.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Slide05
+{
+    public class Student
+    {
+        public string LastName { get; set; }
+        public string Group { get; set; }
+    }
+
+    public class Program
+    {
+        public static void MainX()
+        {
+            var students = new List<Student>
+            {
+                new Student { LastName="Jones", Group="FT-1" },
+                new Student { LastName="Adams", Group="FT-1" },
+                new Student { LastName="Williams", Group="KN-1"},
+                new Student { LastName="Brown", Group="KN-1"}
+            };
+
+            var result = new List<string>();
+            foreach (var s in students)
+                if (s.Group == "KN-1")
+                    result.Add(s.LastName);
+
+            //или
+            result = students.Where(z => z.Group == "KN-1").Select(z => z.LastName).ToList();
+        }
+    }
+}
