4a448c371bed79f31b7299ac884e55dbac0f9e99
okulovsky <yuri.okulovsky@gmail.com>
2015-01-07 00:26:21 +0500

    начал ЖА

---
+++ CS/Lecture23a/S01 - Планирование расписания.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Slide01
+{
+    class Work
+    {
+        public double Start { get; set; }
+        public double End { get; set; }
+    }
+
+
+    public class Program
+    {
+        static IEnumerable<Work> FindSchedule(List<Work> works)
+        {
+            var sortedWorks = works.OrderBy(z => z.End);
+            var currentEnd = double.NegativeInfinity;
+            foreach(var work in sortedWorks)
+                if (work.Start >= currentEnd)
+                {
+                    currentEnd = work.End;
+                    yield return work;
+                }
+        }
+    }
+}
