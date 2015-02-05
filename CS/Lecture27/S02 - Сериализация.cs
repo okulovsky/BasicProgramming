b820d1e3945b7e8f55727aee4a1164c476c6ac1f
okulovsky <yuri.okulovsky@gmail.com>
2015-02-05 13:05:34 +0500

    renamings

---
+++ CS/Lecture27/S02 - Сериализация.cs
@@ -0,0 +1,46 @@
+﻿namespace S02
+{
+	using System;
+	using System.Text;
+	class Enterprise
+	{
+		public string Name { get; set; }
+		public string DirectorName { get; set; }
+		public string RegistrationNumber { get; set; }
+
+	}
+
+	class Person
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+
+	class Program
+	{
+		static void Print(object obj)
+		{
+			var builder = new StringBuilder();
+
+			var type = obj.GetType();
+			foreach (var property in type.GetProperties())
+			{
+				builder.Append(property.Name);
+				builder.Append(" : ");
+				builder.Append(property.GetValue(obj));
+				builder.Append("\n");
+			}
+			var str = builder.ToString();
+			Console.WriteLine(str);
+		}
+
+		public static void Main()
+		{
+			var enterprise = new Enterprise { Name = "Vector", DirectorName = "Jones", RegistrationNumber = "123" };
+			Print(enterprise);
+
+			var person = new Person { FirstName = "William", LastName = "Smith" };
+			Print(person);
+		}
+	}
+}
